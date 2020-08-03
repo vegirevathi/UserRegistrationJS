0e83ab04b956df60bb7a0cf045f80ab5e3352d13
vegirevathi <vegirevathi@gmail.com>
2020-08-03 10:15:08 +0530

    ADDED: Code to check email regex

--- userRegistration.js
+++ userRegistration.js
@@ -1,10 +1,11 @@
 console.log("Welcome to user registration");
 
 const userInput = require('prompt-sync')();
-let NAME_REGEX = new RegExp("^[A-Z]{1}[A-Za-z]{2,}");
+let NAME_REGEX = new RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
+let EMAIL_REGEX = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
 
 function checkValidInput(userInput, regexPattern) {
-	if(NAME_REGEX.test(firstName))
+	if(regexPattern.test(firstName))
 		console.log("Valid");
 	else
 		console.log("Invalid");
@@ -15,3 +16,7 @@ checkValidInput(firstName, NAME_REGEX);
 
 let lastName = userInput("Enter Last Name: ");
 checkValidInput(lastName, NAME_REGEX);
+
+let email = userInput("Enter Email: ");
+checkValidInput(email, EMAIL_REGEX);
+
