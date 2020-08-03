015cfad3834bc28ffe6d5e55b4f409a60c141eff
vegirevathi <vegirevathi@gmail.com>
2020-08-03 10:34:36 +0530

    ADDED: Code to check valid status of password rule 2

--- userRegistration.js
+++ userRegistration.js
@@ -4,10 +4,10 @@ const userInput = require('prompt-sync')();
 let NAME_REGEX = new RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
 let EMAIL_REGEX = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
 let PHONE_NUMBER_REGEX = new RegExp("^[0-9][0-9] [0-9]{10}$");
-let PASSWORD_REGEX = new RegExp("[a-zA-Z0-9@_$#%]{8,}$");
+let PASSWORD_REGEX = new RegExp("^(?=.*?[A-Z])([A-Za-z0-9]{8,})$");
 
 function checkValidInput(userInput, regexPattern) {
-	if(regexPattern.test(firstName))
+	if(regexPattern.test(userInput))
 		console.log("Valid");
 	else
 		console.log("Invalid");
