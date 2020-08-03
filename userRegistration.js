c22024a8def6648bfda824a8dbbf37a8d6fc4367
vegirevathi <vegirevathi@gmail.com>
2020-08-03 10:19:12 +0530

    ADDED: Code to check valid status for phone number

--- userRegistration.js
+++ userRegistration.js
@@ -3,7 +3,7 @@ console.log("Welcome to user registration");
 const userInput = require('prompt-sync')();
 let NAME_REGEX = new RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
 let EMAIL_REGEX = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
-
+let PHONE_NUMBER_REGEX = new RegExp("^[0-9][0-9] [0-9]{10}$");
 function checkValidInput(userInput, regexPattern) {
 	if(regexPattern.test(firstName))
 		console.log("Valid");
@@ -19,4 +19,8 @@ checkValidInput(lastName, NAME_REGEX);
 
 let email = userInput("Enter Email: ");
 checkValidInput(email, EMAIL_REGEX);
+
+let phoneNumber = userInput("Enter phone number");
+checkValidInput(phoneNumber, PHONE_NUMBER_REGEX);
+
 
