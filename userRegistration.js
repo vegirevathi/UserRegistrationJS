4e67c6e09efaceb7496a6750cb7e09838f1293ce
vegirevathi <vegirevathi@gmail.com>
2020-08-03 09:42:48 +0530

    ADDED: Code to check valid conditions for first name

--- userRegistration.js
+++ userRegistration.js
@@ -1,1 +1,10 @@
 console.log("Welcome to user registration");
+
+const userInput = require('prompt-sync')();
+let regex = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
+const firstName = userInput("Enter first Name :  ");
+
+if(regex.test(firstName))
+	console.log("Valid");
+else
+	console.log("Invalid");
