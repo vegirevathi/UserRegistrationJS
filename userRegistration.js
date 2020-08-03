ba37a4dcf0b221cdf40e680b908da0c136b3ae36
vegirevathi <vegirevathi@gmail.com>
2020-08-03 09:50:56 +0530

    ADDED: Code to check valid status for last name

--- userRegistration.js
+++ userRegistration.js
@@ -1,10 +1,17 @@
 console.log("Welcome to user registration");
 
 const userInput = require('prompt-sync')();
-let regex = new RegExp("^[A-Z]{1}[A-Za-z]{2}");
-const firstName = userInput("Enter first Name :  ");
+let NAME_REGEX = new RegExp("^[A-Z]{1}[A-Za-z]{2,}");
 
-if(regex.test(firstName))
-	console.log("Valid");
-else
-	console.log("Invalid");
+function checkValidInput(userInput, regexPattern) {
+	if(NAME_REGEX.test(firstName))
+		console.log("Valid");
+	else
+		console.log("Invalid");
+}
+
+let firstName = userInput("Enter First Name: ");
+checkValidInput(firstName, NAME_REGEX);
+
+let lastName = userInput("Enter Last Name: ");
+checkValidInput(lastName, NAME_REGEX);
