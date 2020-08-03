3f9ff439bccd9338ea6865d659f0dc3b5a27283f
vegirevathi <vegirevathi@gmail.com>
2020-08-03 10:57:44 +0530

    ADDED: More email regex patterns

--- userRegistration.js
+++ userRegistration.js
@@ -2,8 +2,11 @@ console.log("Welcome to user registration");
 
 const userInput = require('prompt-sync')();
 let NAME_REGEX = new RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
-let EMAIL_REGEX = new RegExp("^[a-zA-Z0-9.+_-]+[@][a-zA-Z0-9]+[.]co(m|.in)$");
-let PHONE_NUMBER_REGEX = new RegExp("^[0-9][0-9] [0-9]{10}$");
+let EMAIL_REGEX = new RegExp("^([a-zA-Z0-9_\\-\\+]+)@([a-zA-Z1-9]+)\\.([a-zA-Z]{2,})$" ||
+										"^([a-zA-Z0-9_\\-\\+]+)@([a-zA-Z1-9]+)\\.([a-zA-Z]{2,}+)\\.([A-Za-z]{2})$" || 
+										"^([a-zA-Z0-9_\\-\\+]+)\\.([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z1-9]+)\\.([a-zA-Z]{2,})$" ||
+										"^([a-zA-Z0-9_\\-\\+]+)\\.([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z1-9]+)\\.([a-zA-Z]{2,}+)\\.([A-Za-z]{2})$");
+let PHONE_NUMBER_REGEX = new RegExp("^[1-9][0-9] [0-9]{10}$");
 let PASSWORD_REGEX = new RegExp("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\\s).{8,15}$");
 
 function checkValidInput(userInput, regexPattern) {
